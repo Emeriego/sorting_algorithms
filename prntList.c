5a451f5e813c4661aded16b55b917a6c34fb0c4f
Godwin <emeriego@gmail.com>
2023-12-19 19:59:56 -0800

    corrections

--- prntList.c
+++ prntList.c
@@ -4,19 +4,19 @@
  * prntList - Prints list of ints
  * @list: list to be printed
  */
- void prntList(const listint_t *list)
+void prntList(const listint_t *list)
 {
-  int idx;
+	int idx;
 
-  for (idx = 0; list !== NULL; ++idx)
-  {
-    if (idx > 0)
-    {
-      printf(", ");
-    }
-    printf("%d", list->n);
-    ++idx;
-    list = list->next;
-  }
-  printf("\n");
+	for (idx = 0; list != NULL; ++idx)
+	{
+		if (idx > 0)
+		{
+			printf(", ");
+		}
+		printf("%d", list->n);
+		++idx;
+		list = list->next;
+	}
+	printf("\n");
 }
