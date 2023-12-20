443aebe428814cfa72ed12de2aab9c30af30b93c
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-12-20 11:22:33 +0100

    final commit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,33 @@
 #include "sort.h"
+/**
+ * getMin - Locatin the min From Current index in the array
+ * @array: The Array to be Searched
+ * @index: Starting Index of The Search
+ * @size: The Size of The Array
+ * Return: (int) index of min if found or
+ * same given index if index is the min
+ */
+
+int getMin(int *array, int index, size_t size)
+{
+	int min, idx_min, idx;
+
+	min = array[index];
+	idx_min = index;
+	for (idx = index; idx < (int)size; idx++)
+	{
+		if (array[idx] < min)
+		{
+			min = array[idx];
+			idx_min = idx;
+		}
+	}
+	if (idx_min == index)
+	{
+		return (-2);
+	}
+	return (idx_min);
+}
 
 /**
  * selection_sort - Selection Sort Algrithm
