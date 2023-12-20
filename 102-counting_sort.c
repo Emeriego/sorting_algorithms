d82395f4937ddf0f704071f8b76aebc8ef10d157
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-12-20 11:30:06 +0100

    final commit

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,5 +1,28 @@
 #include "sort.h"
 /**
+ * get_max - Get the maximum value in an array of integers.
+ * @array: Array of integers.
+ * @size: Size of array.
+ *
+ * Return: returns max int in an array.
+ */
+int getMax(int *array, int size)
+{
+	int mx, idx;
+
+    mx = array[0];
+    idx = 1;
+	while (idx < size)
+	{
+		if (array[idx] > mx)
+			mx = array[idx];
+        idx++;
+	}
+
+	return (mx);
+}
+
+/**
  * counting_sort - Sorts an array of integers in ascending order
  * @array: Array of integers.
  * @size: Size of the array.
