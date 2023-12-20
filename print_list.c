bd564d54c8d237012dc6a47c0dad1f159abdbae8
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-12-20 05:18:03 +0100

    final commit

--- print_list.c
+++ print_list.c
@@ -4,11 +4,8 @@
  * print_list - Prints list of ints
  * @list: list to be printed
  */
-<<<<<<< HEAD:prntList.c
-void prntList(const listint_t *list)
-=======
+
  void print_list(const listint_t *list)
->>>>>>> 50a1324dd0e961a38e2c80b34892742ce41d20d3:print_list.c
 {
 	int idx;
 
