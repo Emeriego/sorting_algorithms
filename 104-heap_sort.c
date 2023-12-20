b594cdbedb731d2a25e9bdcb0fb6743363d2589e
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-12-20 11:53:06 +0100

    final commit

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,5 +1,36 @@
 #include "sort.h"
 /**
+* stupif - recurrssive heap
+* @array: Array to be sorted
+* @heap: size of heap data
+* @idx: index
+* @size: size of array
+*/
+void stupif(int *array, int heap, int idx, int size)
+{
+	int lar = idx;
+    int left;
+    int right, tud;
+
+    left = 2 * idx + 1;
+    right = 2 * idx + 2;
+	if (left < heap && array[left] > array[lar])
+    {
+        lar = left;
+    }
+	if (right < heap && array[right] > array[lar])
+		lar = right;
+	if (lar != idx)
+	{
+		tud = array[idx];
+        array[idx] = array[lar];
+        array[lar] = tud;
+		print_array(array, size);
+		stupif(array, heap, lar, size);
+	}
+}
+
+/**
 * heap_sort - Implements heap sort algo
 * @array: array to be sorted
 * @size: Size of array
